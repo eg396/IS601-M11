7e156b3817dc9aee6e16515e5704cfee19f4569d
Evan <eg396@njit.edu>
2025-07-25 04:37:03 -0400

    chore: fix config file

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/fastapi_db"
+    DATABASE_URL: str = "postgresql://postgres:157862@localhost:5432/fastapi_db"
     
     class Config:
         env_file = ".env"
